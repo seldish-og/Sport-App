91c879fff540ff57f4e90a2b30e9aaa77acc90cb
seldish-og <marius@gaius>
2022-10-25 06:17:03 -0400

    done maps module

---
+++ test-prj/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <YandexMap :coordinates="map_coordinates">
+    <YandexMarker :coordinates="marker_coordinates" :marker-id="123">
+      <template #component>
+        <MarkerDescription v-model="name" />
+      </template>
+    </YandexMarker>
+  </YandexMap>
+</template>
+
+<script setup>
+import { YandexMap, YandexMarker } from 'vue-yandex-maps'
+import MarkerDescription from './components/MarkerDescription.vue';
+
+const map_coordinates = [54.512680, 36.271831];
+let marker_coordinates = [54, 36]
+</script>
+
+<style>
+.yandex-container {
+  height: 400px;
+}
+
+.yandex-balloon {
+  height: 200px;
+  width: 200px;
+}
+</style>
---
+++ test-prj/src/components/MarkerDescription.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        Команда: {{ teamValue }}
+        Мероприятие: {{ eventValue }}
+
+    </div>
+  </template>
+  
+  <script setup>
+  import defineProps from 'vue-yandex-maps'
+  defineProps({
+    teamValue: {
+      type: String,
+      default: '',
+    },
+    eventValue: {
+        type: String,
+        default: '',
+    }
+  });
+  
+
+  </script>
